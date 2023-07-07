cd3e839ed84fa5e1105295f52ec5c58936bc9f30
Richard Carrion <rcarrion3406@gmail.com>
2023-07-06 23:02:23 -0500

    Dockerfiel and host to true

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 8080
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

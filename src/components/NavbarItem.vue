1fe6b3573079a2565818b2c8932de303c56860b2
Richard Carrion <rcarrion3406@gmail.com>
2023-07-12 23:11:27 -0500

    Details

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,18 +1,22 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 const props = defineProps({
   link: String,
   label: String,
   icon: String,
-  active: Boolean,
+  active: Boolean
 })
 
-function navigate(){ 
-  window.location.href = props.link; 
+function navigate() {
+  window.location.href = props.link
 }
 </script>
 <template>
-    <div class="bg-black h-7 hover:scale-110 hover:border-b-2 hover:border-b-white hover:px-3 transition ease-in-out hover:cursor-pointer duration-200 hover:mx-5" :class="active ? 'border-b-white border-b-2' : ''" @click="navigate()">
-      <a class="mx-3 text-sm" > <icon class="icon" :icon="icon" /> {{ label }} </a>
-    </div>
+  <div
+    class="bg-black h-7 hover:scale-110 hover:border-b-2 hover:border-b-white hover:px-3 transition ease-in-out hover:cursor-pointer duration-200"
+    :class="active ? 'border-b-white border-b-2' : ''"
+    @click="navigate()"
+  >
+    <a class="mx-3 text-sm"> <icon class="icon" :icon="icon" /> {{ label }} </a>
+  </div>
 </template>
